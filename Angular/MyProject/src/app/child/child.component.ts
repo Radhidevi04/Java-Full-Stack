aab95173fa4a36a80c0739bfea716305fcebf625
Radhidevi04 <radhidevi.kalaiselvan@gmail.com>
2024-07-31 17:55:35 +0530

    files added

--- Angular/MyProject/src/app/child/child.component.ts
+++ Angular/MyProject/src/app/child/child.component.ts
@@ -12,12 +12,12 @@ export class ChildComponent {
     sum : number;
     sub : number;
     mul : number;
-    c : string;
+    a : number;
     constructor(private calc : CalculatorService, private name : NameServiceService ){
       this.sum = calc.getAddition(200,400);
       this.sub = calc.getSubtraction(120,60);
       this.mul = calc.getMultiplication(50,50);
-      this.c = name.getnameservice("Radhi");
+      this.a = name.getnameservice(5);
     }
    }
 
--- Angular/MyProject/src/app/name-service.service.ts
+++ Angular/MyProject/src/app/name-service.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class NameServiceService {
-  getnameservice(a : string){
-    return a;
+  getnameservice(a : number,result : number = 1){
+    for(let i=1;i<=a;i++){
+       result *= i
+    }
+    return result
   }
 }
